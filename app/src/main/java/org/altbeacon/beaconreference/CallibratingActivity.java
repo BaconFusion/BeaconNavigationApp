a7fb12b715ab60dbbb62ef10308f6453efbbb658
f.buschendorf <f.buschendorf@stud.uni-goettingen.de>
2016-09-15 15:55:20 +0200

    new activity works

--- app/src/main/java/org/altbeacon/beaconreference/CallibratingActivity.java
+++ app/src/main/java/org/altbeacon/beaconreference/CallibratingActivity.java
@@ -1,29 +1,17 @@
 package org.altbeacon.beaconreference;
 
 import android.Manifest;
-import android.annotation.TargetApi;
+
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
-import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by fabiola on 15.09.16.
  */
 public class CallibratingActivity extends Activity {
-
+    protected static final String TAG = "CallibratingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +20,14 @@ public class CallibratingActivity extends Activity {
         setContentView(R.layout.activity_callibrating);
 
         //Intent intent = getIntent();
+        logToDisplay("Callibrating just launched");
         //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         //TextView textView = new TextView(this);
         //textView.setTextSize(40);
-        //textView.setText(message);
+        //textView.setText("Callibrating");
 
-        //ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        //ViewGroup layout = (ViewGroup) findViewById(R.id.activity_callibrating);
         //layout.addView(textView);
-
-
     }
 
     @Override
@@ -52,9 +39,17 @@ public class CallibratingActivity extends Activity {
     @Override
     public void onPause() {
         super.onPause();
-        ((BeaconReferenceApplication) this.getApplicationContext()).setMonitoringActivity(null);
+        ((BeaconReferenceApplication) this.getApplicationContext()).setCallibratingActivity(null);
     }
 
-
+    public void logToDisplay(final String line) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                EditText editText = (EditText)CallibratingActivity.this
+                        .findViewById(R.id.callibratingText);
+                editText.append(line+"\n");
+            }
+        });
+    }
 
 }
