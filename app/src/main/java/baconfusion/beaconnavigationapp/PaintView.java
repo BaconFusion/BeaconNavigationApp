47223300efd63c259ac80e867d912f3e7c9506fe
shallee <seeling.wong@gwdg.de>
2016-09-27 12:15:47 +0200

    change draw
    draw axes not in middle,
    scale with min and max from beacons

--- app/src/main/java/baconfusion/beaconnavigationapp/PaintView.java
+++ app/src/main/java/baconfusion/beaconnavigationapp/PaintView.java
@@ -61,32 +61,61 @@ public class PaintView extends View implements PositionNotifier{
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             paint.setColor(Color.BLACK);
-            paint.setStrokeWidth(sw);
+            paint.setStrokeWidth(2.0f);
             paint.setStrokeCap(Paint.Cap.ROUND);
-            // x-Achse
+            /*// x-Achse
             canvas.drawLine(0.0f, heightOfView / 2, widthOfView, heightOfView / 2, paint);
             //y-Achse
-            canvas.drawLine(widthOfView / 2, 0, widthOfView / 2, heightOfView, paint);
-            paint.setStrokeWidth(5.0f);
+            canvas.drawLine(widthOfView / 2, 0, widthOfView / 2, heightOfView, paint);*/
+            sw = 10.0f;
+            //x_achse
+            canvas.drawLine(0.0f + sw, heightOfView - sw, widthOfView - sw, heightOfView - sw, paint);
+            //y_achse
+            canvas.drawLine(0.0f + sw, heightOfView - sw, 0.0f + sw, 0.0f + sw, paint);
+            paint.setStrokeWidth(sw);
             //widthOfView -= sw;
             //heightOfView -= sw;
-            //nochmal gut skalieren
-            float scale_x = (float) widthOfView / 40.0f;
-            float scale_y = (float) heightOfView / 40.0f;
+            //nochmal gut skalieren, skaliere, suche min und max und scale mit max-min
+            float delta_x = delta(b_x);
+            float delta_y = delta(b_y);
+            float scale_x = (float) widthOfView-sw / delta_x; //wd /delta_x
+            float scale_y = (float) heightOfView-sw / delta_y;
             float mid_x = (float) widthOfView / 2.0f;
             float mid_y = (float) heightOfView / 2.0f;
-            paint.setColor(Color.RED);
-            float draw_x = mid_x + ( x*scale_x);
-            float draw_y = mid_y - ( y*scale_y);
-            canvas.drawPoint(draw_x, draw_y, paint);
+            float x_axe = 0.0f + sw;
+            float y_axe = (float) heightOfView - sw;
             //draw Beacons
-            paint.setColor(Color.BLUE);
+            //paint.setColor(Color.BLUE);
             for(int i = 0; i < len; i++) {
-                float x_i = mid_x + ( b_x[i] * scale_x);
-                float y_i = mid_y - ( b_y[i] * scale_y);
+                //float x_i = mid_x + ( b_x[i] * scale_x);
+                //float y_i = mid_y - ( b_y[i] * scale_y);
+                float x_i = x_axe + ( b_x[i] * scale_x);
+                float y_i = y_axe - ( b_y[i] * scale_y);
+                paint.setColor(b_i[i]);
                 canvas.drawPoint(x_i, y_i, paint);
                 String ident = ""+b_i[i];
                 canvas.drawText(ident, x_i, y_i, paint);
             }
+            //draw own position
+            paint.setColor(Color.RED);
+            //float draw_x = mid_x + ( x*scale_x);
+            //float draw_y = mid_y - ( y*scale_y);
+            float draw_x = x_axe + ( x*scale_x);
+            float draw_y = y_axe - ( y*scale_y);
+            canvas.drawPoint(draw_x, draw_y, paint);
+        }
+
+        float delta(float[] x){
+            float max_x = 0;
+            for(int i = 0; i < len; i++){
+                if (max_x < x[i])
+                    max_x = x[i];
+            }
+            float min_x = 0;
+            for(int i = 0; i < len; i++) {
+                if(min_x > x[i])
+                    min_x = x[i];
+            }
+            return max_x - min_x;
         }
 }
